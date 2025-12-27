320474c79c839222fae9bc5ed16a52c0e2d743d5
Oleksii Kunakov <oleksiikunakov@proton.me>
2025-12-27 20:59:45 +0200

    feat: cleaned useless work in tris branch

--- lib/core/di/router_module.dart
+++
@@ -1,8 +0,0 @@
-import 'package:injectable/injectable.dart';
-import 'package:my_rss/core/router/app_router.dart';
-
-@module
-abstract class RouterModule {
-  @singleton
-  AppRouter get appRouter => AppRouter();
-}
